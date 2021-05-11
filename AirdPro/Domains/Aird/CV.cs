5037d79009804b8969892f7b42983f9fafd4783a
LMS <lms19880914@qq.com>
2021-05-11 22:12:34 +0800

    bugfix for Stack ZDPD log on the console

---
+++ AirdPro/Domains/Aird/CV.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using pwiz.CLI.cv;
+using pwiz.CLI.data;
+using pwiz.CLI.msdata;
+
+namespace AirdPro.Domains.Aird
+{
+    public class CV
+    {
+        public string cvid;
+        public object value;
+        public string units;
+
+        public CV()
+        {
+        }
+
+        public CV(CVParam param)
+        {
+            this.cvid = ((int) param.cvid)+":"+param.name;
+            this.value = param.value.ToString();
+            int unitsId = (int) param.units;
+            if (unitsId != -1)
+            {
+                this.units = (int)param.units + ":"+ param.unitsName;
+            }
+        }
+
+        public static List<CV> trans(Spectrum spectrum)
+        {
+            List<CV> cvList = new List<CV>();
+            CVParamList cvParamList = spectrum.cvParams;
+            foreach (var cvParam in cvParamList)
+            {
+                cvList.Add(new CV(cvParam));
+            }
+
+            return cvList;
+        }
+    }
+}
